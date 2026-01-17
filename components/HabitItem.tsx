c1911aed52346749f54841ab8776be32bdd6cd13
Joshua Barker <joshuafbarker@gmail.com>
2026-01-17 16:44:41 -0500

    update habit item style, change 'delete' icon

--- components/HabitItem.tsx
+++ components/HabitItem.tsx
@@ -22,7 +22,7 @@ export default function HabitItem({ id, name, isCompleted, streak }: Props) {
     <li
       key={id}
       className={cn(
-        "mb-2 border rounded p-4 flex min-h-16 items-center",
+        "mb-2 border border-zinc-300 dark:border-zinc-700 rounded p-4 flex min-h-16 items-center",
         isCompleted
           ? "border-green-300 bg-green-50 dark:bg-green-900"
           : "border-slate-300",
@@ -36,13 +36,13 @@ export default function HabitItem({ id, name, isCompleted, streak }: Props) {
         )}
 
         {streak > 0 ? (
-          <span className="text-sm text-slate-500">{streak} day streak</span>
+          <span className="text-sm text-zinc-500">{streak} day streak</span>
         ) : null}
       </div>
 
       {!name || isCompleted ? null : (
         <button
-          className="cursor-pointer mx-1 size-6 rounded-full hover:bg-emerald-200 flex items-center justify-center"
+          className="cursor-pointer mx-1 size-6 rounded-full hover:bg-emerald-500 dark:hover:bg-emerald-900 flex items-center justify-center"
           onClick={() => handleClick(id, "complete")}
         >
           <svg
@@ -64,7 +64,7 @@ export default function HabitItem({ id, name, isCompleted, streak }: Props) {
 
       {!name ? null : (
         <button
-          className="cursor-pointer mx-1 size-6 rounded-full hover:bg-red-200 flex items-center justify-center"
+          className="cursor-pointer mx-1 size-6 rounded-full hover:bg-red-500 dark:hover:bg-red-900 flex items-center justify-center"
           onClick={() => handleClick(id, "delete")}
         >
           <svg
@@ -78,7 +78,7 @@ export default function HabitItem({ id, name, isCompleted, streak }: Props) {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M6 18 18 6M6 6l12 12"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
             />
           </svg>
         </button>
